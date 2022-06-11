ec12f9f968edec902183bff90cccab923cf0f321
Gaurav Kumar <gauravswarankar1@gmail.com>
2022-06-11 19:02:54 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/gaurav/flutter_auto_search/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gaurav.flutter_auto_search
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auto_search/auto_search.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final List<String> names = [
+    "Ayden Ram",
+    "Rowan Trevon",
+    "Garrison Faisal",
+    "Bridie Ford",
+    "Rameel Xavier",
+    "Abriel Yestin",
+    "Cal Heston",
+    "Ryland Nick",
+    "Orson Kaylen",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      child: AutoSearchInput(
+        data: names,
+        maxElementsToDisplay: 10,
+        onItemTap: (index) {},
+      ),
+    );
+  }
+}
+
+
